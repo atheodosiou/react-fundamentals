7c5ccc27ae0356fb7b24934066e9e1df80eff1b1
Anastasios Theodosiou <anastasios.theodosiou@gmail.com>
2020-02-24 16:16:10 +0200

    Create countries app and install node-sass in order to use scss instedad of css files

--- react-views-components/src/App.js
+++ react-views-components/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 // import textView from './assets/views/textView/textView';
 import ClickCounter from './assets/components/ClickCounter/ClickCounter';
 
